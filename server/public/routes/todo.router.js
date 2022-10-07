bd958dcd0646c25f4f168332d37d175f77f8c250
Sam Phipps <samuel.v.phipps@gmail.com>
2022-10-07 11:43:28 -0500

    get info and connect to postgress

--- server/public/routes/todo.router.js
+++ server/public/routes/todo.router.js
@@ -9,7 +9,18 @@ router.get('/', (req,res)=>{
     //log
     console.log('in todo GET');
     //pool query
-
+    let queryText=`
+        SELECT * FROM "tasks"
+        ORDER BY "completed";
+    `
+    pool.query(queryText)
+        .then(result=>{
+            res.send(result.rows);
+        })
+        .catch(err=>{
+            console.log('error getting tasks', err);
+            res.sendStatus(500);
+        });
 })
 
 //post tasks
